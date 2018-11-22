631165152fd420477bbaa4cba251a679182d7672
Angus Fong <fongangus@gmail.com>
2018-11-21 20:36:20 -0500

    Update query.py

--- query.py
+++ query.py
@@ -1,22 +1,32 @@
 """Example of Python client calling Knowledge Graph Search API."""
+#types allowed:
+#EducationalOrganization, Event, GovernmentOrganization, LocalBusiness, Movie, MovieSeries, MusicAlbum,
+#MusicGroup, MusicRecording, Organization,Periodical,Person,Place,SportsTeam,TVEpisode,TVSeries,VideoGame,
+#VideoGameSeries,WebSite
+
 import json
 import urllib
 import urllib.parse
 import urllib.request
 
-api_key = open('.api_key').read()
-query = 'Taylor Swift'
+LIMIT = 100
+API_KEY = open('.api_key').read()
+
+query = 'chess'
 service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
 params = {
     'query': query,
-    'limit': 10,
+    'limit': LIMIT,
     'indent': True,
-    'key': api_key,
-    'types': 'Person'
+    'key': API_KEY,
+    'types': 'Movie'
 }
+
 url = service_url + '?' + urllib.parse.urlencode(params)
 response = json.loads(urllib.request.urlopen(url).read())
 for element in response['itemListElement']:
-    print(element.keys())
     print(element['result'].keys())
+    print(element['result']['@id'])
     print(element['result']['name'] + ' (' + str(element['resultScore']) + ')')
+
+    
