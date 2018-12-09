2218f6dd757f13fa5ef47b78e3c65bc3f260a316
Angus Fong <fongangus@gmail.com>
2018-12-09 18:07:41 -0500

    Update query.py

--- query.py
+++ query.py
@@ -5,14 +5,19 @@
 #VideoGameSeries,WebSite
 
 import json
+import numpy as np
+import requests
+import sys
 import urllib
 import urllib.parse
 import urllib.request
+import get_wikidata
 
-LIMIT = 100
+LIMIT = 100 #this many?
 API_KEY = open('.api_key').read()
 
-query = input("Search for: ")
+#prompt user to input topic of interest (easy for debugging)
+query = input("Topic of interest: ")
 service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
 params = {
     'query': query,
@@ -22,11 +27,30 @@ params = {
     'types': 'Thing'
 }
 
-url = service_url + '?' + urllib.parse.urlencode(params)
-response = json.loads(urllib.request.urlopen(url).read())
-for element in response['itemListElement']:
-    print(element['result'].keys())
-    print(element['result']['@id'])
-    print(element['result']['name'] + ' (' + str(element['resultScore']) + ')')
+#loop until successfully get response (might take some time depending on server)
+while True:
+    try:
+        response = json.loads(urllib.request.urlopen(url).read())
+        break
+    except Exception: # still allows to quit with KeyboardInterrupt
+        continue
 
-    
+
+#array of all search results
+ids = []
+names = []
+scores = []
+for element in response['itemListElement']:
+    names.append(element['result']['name'])
+    ids.append(element['result']['@id'][3:]) #this gets freebase id
+    scores.append(element['resultScore'])
+
+#define probability distribution over search results
+probs = [score/sum(scores) for score in scores]
+n = len(scores)
+
+#stay on topic before user terminates
+while True:
+    ind = np.random.choice(n, p=probs)
+    id = ids[ind]
+    get_wikidata.get_wikidata(id)
