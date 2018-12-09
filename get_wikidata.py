70468a48822f1b738ce0a8630318b470d1b4a651
Angus Fong <fongangus@gmail.com>
2018-12-09 18:07:56 -0500

    Update get_wikidata.py

--- get_wikidata.py
+++ get_wikidata.py
@@ -1,23 +1,34 @@
-#input: variable id
+import json
+import numpy as np
+import requests
+import sys
+import urllib
+import urllib.parse
+import urllib.request
 
-url = 'https://query.wikidata.org/sparql'
-query = """
-SELECT ?prop_id ?prop_label ?prop_val ?prop_val_label WHERE {
-  ?interested wdt:P646 \"""" + id + """\". 
-  ?interested ?prop_id ?interested_item.
-  ?wd wikibase:directClaim ?prop_id.
-  ?wd rdfs:label ?prop_label.
-  OPTIONAL {
-    ?interested_item rdfs:label ?prop_val.
-    FILTER((LANG(?prop_val)) = "en")
+def get_wikidata(id):
+  url = 'https://query.wikidata.org/sparql'
+  query = """
+  SELECT ?prop_id ?prop_label ?prop_val ?prop_val_label WHERE {
+    ?interested wdt:P646 \"""" + id + """\". 
+    ?interested ?prop_id ?interested_item.
+    ?wd wikibase:directClaim ?prop_id.
+    ?wd rdfs:label ?prop_label.
+    OPTIONAL {
+      ?interested_item rdfs:label ?prop_val.
+      FILTER((LANG(?prop_val)) = "en")
+    }
+    BIND(COALESCE(?prop_val, ?interestedItem) AS ?prop_val_label)
+    FILTER((LANG(?prop_label)) = "en")
   }
-  BIND(COALESCE(?prop_val, ?interestedItem) AS ?prop_val_label)
-  FILTER((LANG(?prop_label)) = "en")
-}
-"""
-r = requests.get(url, params = {'format': 'json', 'query': query})
-data = r.json()['results']['bindings'] 
-data = [dat for dat in data if len(dat.keys())==4]
+  """
+  r = requests.get(url, params = {'format': 'json', 'query': query})
+  data = r.json()['results']['bindings'] 
+  data = [dat for dat in data if len(dat.keys())==4]
 
-property_ind = np.random.choice(len(data))
-data[property_ind]
+  property_ind = np.random.choice(len(data))
+  data[property_ind]
+  
+  #generate distractors:
+  #current way: get entities of that property type
+  #write sparql code
