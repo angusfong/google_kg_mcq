a11b988562b1796507f2536364908fe64c3dd06e
tomoe-mizutani <32919205+tomoe-mizutani@users.noreply.github.com>
2018-12-11 13:35:08 +0900

    Create q_generator.py

---
+++ q_generator.py
@@ -0,0 +1,25 @@
+# generate a question that asks the property of entity
+def q_generate(entity, property):
+    tags = tag_property(property)
+
+    # super naive but for now: if the last POS is noun
+    if tags[-1][1] == 'NN':
+        print("The " + property + " of " + entity + " is ___")
+    else:
+        print(entity + " is " + property + " ___")
+
+    #https://www.nltk.org/book/ch07.html
+    # grammar = "NP: {<DT>?<JJ>*<NN>}"
+    #
+    # cp = nltk.RegexpParser(grammar)
+    # print(cp)
+    #
+    # result = cp.parse(tags)
+
+
+# takes an English input sentence and tags each word by POS
+# for now, need: Stanford POS tagger https://nlp.stanford.edu/software/tagger.shtml
+def tag_property(sentence):
+    postagger = StanfordPOSTagger("stanford-postagger/models/english-bidirectional-distsim.tagger", "stanford-postagger/stanford-postagger.jar")
+    tags  = postagger.tag(sentence.split())
+    return tags
