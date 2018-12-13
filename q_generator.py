2808edfbba86a2bea9ac5e2b465ffb0e046fc159
tomoe-mizutani <32919205+tomoe-mizutani@users.noreply.github.com>
2018-12-14 06:03:22 +0900

    Update q_generator.py

--- q_generator.py
+++ q_generator.py
@@ -1,9 +1,18 @@
+import sys
+import os
+from os.path import join
 import nltk
 from nltk.tag.stanford import StanfordPOSTagger
 
 # generate a question that asks the property of entity
 def q_generate(entity, property):
-    tags = tag_property(property)
+    try:
+       tagger = os.environ.get("TAGGER")
+    except KeyError:
+       print ("Please set export TAGGER=$PATH/TO/TAGGER")
+       sys.exit(1)
+        
+    tags = tag_property(property, tagger)
 
     # if last tag is not preposition or subordinating conjunction
     if tags[-1][1] != 'IN':
@@ -13,7 +22,7 @@ def q_generate(entity, property):
 
 # takes an English input sentence and tags each word by POS
 # for now, need: Stanford POS tagger https://nlp.stanford.edu/software/tagger.shtml
-def tag_property(sentence):
-    postagger = StanfordPOSTagger("stanford-postagger/models/english-bidirectional-distsim.tagger", "stanford-postagger/stanford-postagger.jar")
+def tag_property(sentence, tagger):
+    postagger = StanfordPOSTagger(join(tagger, "stanford-postagger/models/english-bidirectional-distsim.tagger"), join(tagger, "stanford-postagger/stanford-postagger.jar"))
     tags  = postagger.tag(sentence.split())
     return tags
