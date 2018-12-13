97e1ccf35d87662dcc4e05192041e7b21abb349e
tomoe-mizutani <32919205+tomoe-mizutani@users.noreply.github.com>
2018-12-13 15:42:37 +0900

    Update q_generator.py

--- q_generator.py
+++ q_generator.py
@@ -5,20 +5,11 @@ from nltk.tag.stanford import StanfordPOSTagger
 def q_generate(entity, property):
     tags = tag_property(property)
 
-    # super naive but for now: if the last POS is noun
-    if tags[-1][1] == 'NN':
+    # if last tag is not preposition or subordinating conjunction
+    if tags[-1][1] != 'IN':
         print("The " + property + " of " + entity + " is ___")
     else:
         print(entity + " is " + property + " ___")
-
-    #https://www.nltk.org/book/ch07.html
-    # grammar = "NP: {<DT>?<JJ>*<NN>}"
-    #
-    # cp = nltk.RegexpParser(grammar)
-    # print(cp)
-    #
-    # result = cp.parse(tags)
-
 
 # takes an English input sentence and tags each word by POS
 # for now, need: Stanford POS tagger https://nlp.stanford.edu/software/tagger.shtml
